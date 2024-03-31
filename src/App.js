25f1c1fdaafa99965c0c683b1674f37f3da2f71d
claudiaaziz <claudiahannahh@gmail.com>
2024-03-31 09:49:38 -0400

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return <div className="App">
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/cart" />
+      </Routes>
+    </Router>
+  </div>;
+}
+
+export default App;
